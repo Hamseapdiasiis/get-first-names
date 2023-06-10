cfaf6440c4c032715ce8471cde5cef2e2954d39b
Hamseapdiasiis <hamse906@gmail.com>
2023-06-10 16:11:55 +0300

    Exercise done

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // Good Luck 💪🏾
+function getFirstNames(names){
+    for(let i=0; i<getFirstNames.length; i++){
+        console.log(names[i][0])
+        console.log(names[1][0])
+        console.log(names[2][0])
+    }
+}
+
+
+getFirstNames([["Ahmed","Amina","Aisha"],["Jamac","Khadija","Layla"],["Mohamed","Ali"]])
